083a58ba148874f6bb68b129494feb4fe4f3b47d
Alejandro Arouesty <A01782691@tec.mx>
2023-02-28 12:51:50 -0600

    added things to the game test

--- Videogame_1/Assets/Scripts/CreateBalls.cs
+++ Videogame_1/Assets/Scripts/CreateBalls.cs
@@ -6,18 +6,23 @@ public class CreateBalls : MonoBehaviour
 {
     // Class variable to be changed from the Unity interface
     [SerializeField] GameObject ball;
+      [SerializeField] float delay;
 
     // Start is called before the first frame update
     void Start()
     {
         // Call the 'DropBall' function after 0.5s
         // and then keep calling it every 0.5s
-        InvokeRepeating("DropBall", 0.5f, 0.8f);
+        InvokeRepeating("DropBall", 0.5f, delay);
     }
 
     void DropBall()
     {
+        // Generate a new random position
+        Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), 14, 0);
         // Crete a copy of the prefab
-        Instantiate(ball);
+        GameObject obj = Instantiate(ball, pos, Quaternion.identity);
+        // Doom the object to die in 5s
+        Destroy(obj, 5);
     }
 }
